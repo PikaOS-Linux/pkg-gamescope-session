d8b02e9f63bcc468685cec731a434af19689aa86
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-07-15 15:01:42 +0300

    Update packging to new repo

--- main.sh
+++ main.sh
@@ -1,17 +1,15 @@
 DEBIAN_FRONTEND=noninteractive
 
 # Clone Upstream
-git clone https://github.com/KyleGospo/gamescope-session
+git clone https://github.com/PikaOS-linux/gamescope-session
 cp -rvf ./debian ./gamescope-session
 cd ./gamescope-session
-patch -Np1 -i ./fedora.patch
-patch -Np1 -i ../patches/0001-update-to-work-on-nobara-with-hdr.patch
 
 # Get build deps
 apt-get build-dep ./ -y
 
 # Build package
-dpkg-buildpackage
+dpkg-buildpackage --no-sign
 
 # Move the debs to output
 cd ../
